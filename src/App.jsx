cb97133439693368ef60491fa762ae3d45b5aca7
sonia <soniayjw@gmail.com>
2025-04-25 18:00:32 +0800

    feat: Day44  React 路由練習 (2) - React Router Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css';
-import { HashRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { HashRouter, NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 
+const LogOut = () => {
+  const navigate = useNavigate();
+  return (
+    <button onClick={() => navigate('/login')}>登出</button>
+  )
+}
 const Todo = () => {
-  return <p>這是 Todo 頁面</p>;
+  return (
+    <>
+      <p>這是 Todo 頁面</p>
+      <LogOut />
+    </>
+  );
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
